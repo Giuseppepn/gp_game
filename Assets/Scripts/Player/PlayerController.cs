8d1074c1ae6f40e2c4a8ebc0a17fd04ed50aa231
Giuseppepn <giuseppe.pereiraneiva.studenti@isii.it>
2024-01-24 20:49:11 +0100

    first
    first

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 6f;
+
+    private PlayerInputs playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+    private Sprite chestOpened;
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputs();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        //patata
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        checkDirection();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+
+    private void checkDirection()
+    {
+        if (movement.x < 0)
+        {
+            
+            mySpriteRender.flipX = true;
+        }
+        else if (movement.x > 0)
+        {
+            
+            mySpriteRender.flipX = false;
+        }
+    }
+
+
+    public void checkChestCollision()
+    {
+
+    }
+
+
+}

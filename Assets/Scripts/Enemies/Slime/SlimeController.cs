322cdbc374041b22ab7e8049494417594e32b2ab
Giuseppepn <giuseppe.pereiraneiva.studenti@isii.it>
2024-01-29 13:31:05 +0100

    SlimeController
    Slime moving alone

--- Assets/Scripts/Enemies/Slime/SlimeController.cs
+++ Assets/Scripts/Enemies/Slime/SlimeController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class SlimeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float moveSpeed = 2f;
+    private Rigidbody2D rb;
+    private Vector2 moveDir;
+
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.deltaTime));
+
     }
+
+    public void MoveTo(Vector2 pos)
+    {
+        moveDir = pos;
+    }
+
+
 }
